0a34831483c0926631bc3345d6dd3315cb9636ba
Alex.Bordeaux <sanek33333@gmail.com>
2023-08-31 23:28:30 +0300

    SM2D-6 Finaly integration between game to ECS is ready

--- src/Components/TransformComponent.h
+++ src/Components/TransformComponent.h
@@ -1,15 +1,18 @@
 #ifndef TRANSFORMCOMPONENT_H
 #define TRANSFORMCOMPONENT_H
 
-#include <glm/glm.hpp>
+#include "glm/glm.hpp"
 
 struct TransformComponent{
-    glm::vec2 m_position;
-    glm::vec2 m_scale;
+    glm::vec2 m_position{};
+    glm::vec2 m_scale{};
     double m_rotation;
 
-    TransformComponent(glm::vec2 position, glm::vec2 scale, double rotation):
-    m_position(position),m_rotation(rotation),m_scale(scale){};
+  explicit TransformComponent(glm::vec2 position = glm::vec2(0, 0), glm::vec2 scale = glm::vec2(1, 1), double rotation = 0.0) {
+    m_position = position;
+    m_scale = scale;
+    m_rotation = rotation;
+  }
 };
 
 #endif
