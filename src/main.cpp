e2eedafe03a3118c743237403eb7555c82cd10c8
LAPTOP-GJIF7K83\sanek <sanek33333@gmail.com>
2023-09-29 21:13:52 +0300

    Fixed compitability problem

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "./Game/Game.h"
 #include "Logger/logger.h"
+#include <SDL.h>
 
-int main(int argc,char* argv[]) {
+int main(int argc, char* argv[]) {
   Logger::Initialize();
   auto game = std::make_unique<Game>();
   game->Initialize();
   game->Run();
   game->Destroy();
+  return 0;
 }
